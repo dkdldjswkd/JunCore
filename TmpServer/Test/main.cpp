efb9d07fed2cac2cb99679d81618c8a1fabc627a
dkdldjswkd <dkdldjswkd@naver.com>
2024-10-04 22:26:26 +0900

    circular_buffer test code

--- TmpServer/Test/main.cpp
+++ TmpServer/Test/main.cpp
@@ -1,12 +1,19 @@
-#include <boost/asio.hpp>
+#include <boost/circular_buffer.hpp>
 #include <iostream>
 
-using boost::asio::ip::tcp;
-using namespace std;
+int main() {
+    // 용량이 5인 circular_buffer 생성
+    boost::circular_buffer<int> cb(5);
 
-int main(int argc, char* argv[]) 
-{
-	boost::asio::io_context io_context;
-	boost::asio::ip::tcp::acceptor acceptor(io_context);  
-	cout << "asdf" << endl;
+    // 3개의 요소 추가
+    cb.push_back(1);
+    cb.push_back(2);
+    cb.push_back(3);
+
+    // 남은 사이즈 출력
+    std::cout << "남은 사이즈: " << cb.reserve() << std::endl; // 출력: 2
+    std::cout << "사용 사이즈: " << cb.size() << std::endl; // 출력: 3
+    std::cout << "총 용량: " << cb.capacity() << std::endl; // 출력: 5
+
+    return 0;
 }

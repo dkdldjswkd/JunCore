9f020e990c0df2cf39fd4673b7f9d98874152b18
dkdldjswkd <66017663+dkdldjswkd@users.noreply.github.com>
2024-10-08 14:50:00 +0900

    protobuf build test

---
+++ OtherProjects/TmpServer/ProtobufTest/main.cpp
@@ -0,0 +1,10 @@
+﻿ #include <iostream>
+#include <google/protobuf/port_def.inc>
+using namespace std;
+
+int main()
+{
+	cout << "123" << endl;
+	//req.set_channel_id(1);
+	//cout << req.ch << endl;
+}
